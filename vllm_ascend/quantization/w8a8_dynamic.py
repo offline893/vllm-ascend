040a1ea548f096052a919f9361b56e6847469f9e
wanghanqing_lyt <wanghanqing3@huawei.com>
2025-07-11 10:37:23 +0800

    dynamic eplb

--- vllm_ascend/quantization/w8a8_dynamic.py
+++ vllm_ascend/quantization/w8a8_dynamic.py
@@ -326,14 +326,15 @@ def fused_experts_with_mc2(
 
     hidden_states = torch_npu.npu_moe_distribute_combine(**kwargs_mc2)
 
+    group_list_type = 1
     if shared_experts is None:
-        return hidden_states
+        return hidden_states, expert_token_nums, group_list_type
     else:
         with npu_stream_switch("moe_secondary", 0):
             npu_wait_tensor(shared_act, down_out_list)
             shared_output, _ = shared_experts.down_proj(
                 (shared_act, swiglu_out_scale))
-        return hidden_states, shared_output
+        return hidden_states, shared_output, expert_token_nums, group_list_type
 
 
 def fused_prefill_experts_with_mc2(
@@ -409,18 +410,22 @@ def fused_prefill_experts_with_mc2(
         end_indx += hidden_states_chunk.shape[0]
         if shared_experts is None:
             hidden_states_outputs[start_indx:end_indx,
-                                  ...] = prefill_expert_outputs
+                                  ...] = prefill_expert_outputs[0]
+            expert_token_nums = prefill_expert_outputs[1]
+            group_list_type = prefill_expert_outputs[2]
         else:
             hidden_states_outputs[start_indx:end_indx,
                                   ...] = prefill_expert_outputs[0]
             shared_outputs[start_indx:end_indx,
                            ...] = prefill_expert_outputs[1]
+            expert_token_nums = prefill_expert_outputs[2]
+            group_list_type = prefill_expert_outputs[3]
         start_indx = end_indx
 
     if shared_experts is None:
-        return hidden_states_outputs
+        return hidden_states_outputs, expert_token_nums, group_list_type
     else:
-        return hidden_states_outputs, shared_outputs
+        return hidden_states_outputs, shared_outputs, expert_token_nums, group_list_type
 
 
 # currently expert parallelism implemented with all2all
@@ -551,7 +556,7 @@ def fused_experts_with_all2all(hidden_states: torch.Tensor,
         )
     if len(original_shape) == 3:
         final_hidden_states = final_hidden_states.view(original_shape)
-    return final_hidden_states
+    return final_hidden_states, expert_tokens, group_list_type
 
 
 def fused_experts(hidden_states: torch.Tensor,
@@ -665,7 +670,7 @@ def fused_experts(hidden_states: torch.Tensor,
 
     if len(original_shape) == 3:
         final_hidden_states = final_hidden_states.view(original_shape)
-    return final_hidden_states
+    return final_hidden_states, expert_tokens, group_list_type
 
 
 class AscendW8A8DynamicLinearMethod:
