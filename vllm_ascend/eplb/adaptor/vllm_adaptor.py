541e4b6a52093baad8e78d5af59c353ad4837683
offline0806 <z00858301@china.huawei.com>
2025-09-16 11:42:40 +0800

    [EPLB]Initializing eplb when dynamic eplb.

--- vllm_ascend/eplb/adaptor/vllm_adaptor.py
+++ vllm_ascend/eplb/adaptor/vllm_adaptor.py
@@ -187,8 +187,6 @@ class VllmEplbAdaptor(EplbAdaptor):
 
                 record["layer_list"].append(layer_record)
 
-            print(record)
-
             with open(expert_map_record_path, "w") as f:
                 json.dump(record, f, indent=4)
 
@@ -201,7 +199,6 @@ class VllmEplbAdaptor(EplbAdaptor):
         for expert_tensor, buffer_tensor in zip(
                 self.expert_param_per_layer[layer_id][local_expert_to_replace],
                 self.buffer_tensor_list[buffer_tensor_id]):
-            # expert_tensor.copy_(buffer_tensor)
             expert_tensor = buffer_tensor.clone()
 
     def do_update_log2phy_map(self, layer_id, updated_log2phy_map):
